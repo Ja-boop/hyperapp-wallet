24c43057ec609108a1887b4455c7c6b3ed8c5b8d
Ja-boop <agusmisiones@gmail.com>
2022-01-06 23:47:17 -0300

    S

--- cypress/integration/app.test.js
+++ cypress/integration/app.test.js
@@ -1,6 +1,6 @@
 describe("e2e test MVP", () => {
   it("makes sure theres an xbull button and it redirects to a differente page", () => {
-    cy.visit("http://localhost:8080");
+    cy.visit("https://localhost:8080");
     cy.get("button").contains("Connect with xBull");
   });
 });
