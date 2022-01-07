b7409f1eadc48a5dce41049a82b2982b9cbab13b
RivarolaNicolas <RivarolaNicolas94@gmail.com>
2022-01-06 23:08:53 -0300

    Update app.test.js

--- cypress/integration/app.test.js
+++ cypress/integration/app.test.js
@@ -1,6 +1,6 @@
 describe("e2e test MVP", () => {
   it("makes sure theres an xbull button and it redirects to a differente page", () => {
-    cy.visit("https://localhost:8081");
+    cy.visit("http://localhost:8080");
     cy.get("button").contains("Connect with xBull").click();
     cy.get("div").should("include.text", "XBULL");
   });
